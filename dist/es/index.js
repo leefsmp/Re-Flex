2d6456b4f7214f8fa9d442bc3a1685da29e076aa
Philippe Leefsma <philippe.leefsma@gmail.com>
2017-11-15 16:05:24 -0800

    v 2.0.11 update

--- dist/es/index.js
+++
@@ -1,5 +0,0 @@
-import ReflexContainer from './ReflexContainer';
-import ReflexSplitter from './ReflexSplitter';
-import ReflexElement from './ReflexElement';
-
-export { ReflexContainer, ReflexSplitter, ReflexElement };
